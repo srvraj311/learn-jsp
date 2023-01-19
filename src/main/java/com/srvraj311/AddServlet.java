bdd9b175f0de96361f4971d1855120b3f8bc8ffa
srvraj311 <sourabh.s@iqvia.com>
2023-01-19 16:07:18 +0530

    Added servlet for Adding two numbers and Edited web.xml for servlet mappings

---
+++ src/main/java/com/srvraj311/AddServlet.java
@@ -0,0 +1,35 @@
+package com.srvraj311;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet  {
+
+//	@Override
+//	protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k = i + j;
+//		PrintWriter out = res.getWriter();
+//		out.println("Result is : " + k);
+//		
+//	} 
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		PrintWriter out = res.getWriter();
+		out.println("Result is : " + k);
+		
+	}
+	
+}
